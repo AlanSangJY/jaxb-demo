fc01806abb42d97143fbdccf32874bebd1c791ee
AlanJY Shang <AlanJYShang@axisoft.com>
2023-07-26 17:05:17 +0800

    add validate

--- README.md
+++ README.md
@@ -449,3 +449,106 @@ public class UnitTest {
     }
 }
 ```
+
+## Data validate
+```java
+package com.alan;
+
+import com.alan.entity.schemas.Person;
+import com.alan.schemas.Book;
+import com.alan.schemas.ObjectFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.*;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class UnitTest {
+
+    private final static String BOOK_XML_PATH = "D:\\code\\icode\\jaxb-demo\\src\\main\\resources\\schemas\\xml\\book.xml";
+
+    private final static String BOOK_XSD_PATH = "D:\\code\\icode\\jaxb-demo\\src\\main\\resources\\schemas\\xsd\\book.xsd";
+
+    @Test
+    void validator() throws JAXBException, SAXException, IOException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new File(BOOK_XSD_PATH));
+        Validator validator = schema.newValidator();
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Book book = new Book();
+        book.setName("123456");
+        validator.validate(new JAXBSource(jaxbContext, book));
+    }
+
+    @Test
+    void validatorWithErrorHandle() throws JAXBException, SAXException, IOException {
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        Validator validator = schema.newValidator();
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Book book = new Book();
+        book.setName("123456");
+        validator.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                System.out.println("My warning");
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                System.out.println("My error");
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                System.out.println("My fatal error");
+                exception.printStackTrace();
+            }
+        });
+        validator.validate(new JAXBSource(jaxbContext, book));
+    }
+
+    @Test
+    void validateWhenUnmarshal() throws JAXBException, SAXException, JsonProcessingException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        unmarshaller.setSchema(schema);
+        unmarshaller.setEventHandler(event -> {
+            System.out.println(event.getMessage());
+            return true;
+        });
+        Object unmarshal = unmarshaller.unmarshal(new File(BOOK_XML_PATH));
+        System.out.println(new ObjectMapper().writeValueAsString(unmarshal));
+    }
+
+    @Test
+    void validateWhenMarshal() throws JAXBException, SAXException, JsonProcessingException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        marshaller.setSchema(schema);
+        marshaller.setEventHandler(event -> {
+            System.out.println(event.getMessage());
+            return true;
+        });
+        Book book = new Book();
+        book.setAuth("aaa");
+        marshaller.marshal(book, System.out);
+    }
+}
+```
--- src/test/java/com/alan/UnitTest.java
+++ src/test/java/com/alan/UnitTest.java
@@ -6,15 +6,27 @@ import com.alan.schemas.ObjectFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.*;
+import javax.xml.bind.util.JAXBSource;
 import javax.xml.transform.Result;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 
 public class UnitTest {
+
+    private final static String BOOK_XML_PATH = "D:\\code\\icode\\jaxb-demo\\src\\main\\resources\\schemas\\xml\\book.xml";
+
+    private final static String BOOK_XSD_PATH = "D:\\code\\icode\\jaxb-demo\\src\\main\\resources\\schemas\\xsd\\book.xsd";
 
     @Test
     void unmarshalDocument() throws JAXBException, JsonProcessingException {
@@ -22,7 +34,7 @@ public class UnitTest {
         //JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
         JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Book book = (Book) unmarshaller.unmarshal(new File("D:\\code\\icode\\jaxb-demo\\src\\main\\resources\\schemas\\xml\\book.xml"));
+        Book book = (Book) unmarshaller.unmarshal(new File(BOOK_XML_PATH));
         System.out.println(new ObjectMapper().writeValueAsString(book));
     }
 
@@ -57,4 +69,72 @@ public class UnitTest {
         });
     }
 
+    @Test
+    void validator() throws JAXBException, SAXException, IOException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(new File(BOOK_XSD_PATH));
+        Validator validator = schema.newValidator();
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Book book = new Book();
+        book.setName("123456");
+        validator.validate(new JAXBSource(jaxbContext, book));
+    }
+
+    @Test
+    void validatorWithErrorHandle() throws JAXBException, SAXException, IOException {
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        Validator validator = schema.newValidator();
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Book book = new Book();
+        book.setName("123456");
+        validator.setErrorHandler(new ErrorHandler() {
+            @Override
+            public void warning(SAXParseException exception) throws SAXException {
+                System.out.println("My warning");
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void error(SAXParseException exception) throws SAXException {
+                System.out.println("My error");
+                exception.printStackTrace();
+            }
+
+            @Override
+            public void fatalError(SAXParseException exception) throws SAXException {
+                System.out.println("My fatal error");
+                exception.printStackTrace();
+            }
+        });
+        validator.validate(new JAXBSource(jaxbContext, book));
+    }
+
+    @Test
+    void validateWhenUnmarshal() throws JAXBException, SAXException, JsonProcessingException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        unmarshaller.setSchema(schema);
+        unmarshaller.setEventHandler(event -> {
+            System.out.println(event.getMessage());
+            return true;
+        });
+        Object unmarshal = unmarshaller.unmarshal(new File(BOOK_XML_PATH));
+        System.out.println(new ObjectMapper().writeValueAsString(unmarshal));
+    }
+
+    @Test
+    void validateWhenMarshal() throws JAXBException, SAXException, JsonProcessingException {
+        JAXBContext jaxbContext = JAXBContext.newInstance(Book.class);
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File(BOOK_XSD_PATH));
+        marshaller.setSchema(schema);
+        marshaller.setEventHandler(event -> {
+            System.out.println(event.getMessage());
+            return true;
+        });
+        Book book = new Book();
+        book.setAuth("aaa");
+        marshaller.marshal(book, System.out);
+    }
 }
